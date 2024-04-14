749b14bbec5bd670c72895cf89b8b636038d9a5e
rlafovj <rlafovj@gmail.com>
2024-04-14 16:44:32 +0900

    Feat : Test

---
+++ Demo/src/test/java/com/linus/api/article/service/ArticleServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.linus.api.article.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleServiceImplTest {
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void deleteById() {
+  }
+
+  @Test
+  void findAll() {
+  }
+}
